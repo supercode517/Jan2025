b111a45e28ddbc25887743dd368fe6be5a951871
richard lewis <richardlewisgood626@gmail.com>
2025-01-10 10:45:11 -0800

    Add files via upload

---
+++ 1.10.py
@@ -0,0 +1,38 @@
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
+def load_dataset():
+ (trainX, trainY), (testX, testY) = fashion_mnist.load_data()
+ trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+ testX = testX.reshape((testX.shape[0], 28, 28, 1))
+ trainY = to_categorical(trainY)
+ testY = to_categorical(testY)
+ return trainX, trainY, testX, testY
+def prep_pixels(train, test):
+ train_norm = train.astype('float32')
+ test_norm = test.astype('float32')
+ train_norm = train_norm / 255.0
+ test_norm = test_norm / 255.0
+ return train_norm, test_norm
+def define_model():
+ model = Sequential()
+ model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+ model.add(MaxPooling2D((2, 2)))
+ model.add(Flatten())
+ model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+ model.add(Dense(10, activation='softmax'))
+ opt = SGD(learning_rate=0.01, momentum=0.9)
+ model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+ return model
+def run_test_harness():
+ trainX, trainY, testX, testY = load_dataset()
+ trainX, testX = prep_pixels(trainX, testX)
+ model = define_model()
+ model.fit(trainX, trainY, epochs=10, batch_size=32, verbose=0)
+ model.save('final_model.h5')
+run_test_harness()
